fe31c18ca046f36883eafe9ec707488798ade1c8
Mansi <61620202+mansi230386@users.noreply.github.com>
2020-09-13 00:10:00 +0530

    Delete online_docker_smoke_local.sh

--- scripts/online_docker_smoke_local.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm -v ${PWD}/../env:/etc/env \
-    -v ${PWD}/../report:/etc/report \
-    -t postman/newman run \
-    https://www.getpostman.com/collections/2aa7f23ee76441fcd16c \
-    --environment="/etc/env/docker.postman_environment.json" \
-    --reporters junit --reporter-junit-export="/etc/report/Smoke-report.xml"
